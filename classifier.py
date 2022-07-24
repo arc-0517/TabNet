458cc04f639676cd462ac299754caafe20bb0287
keewon <keewon7@gmail.com>
2022-07-24 19:06:16 +0900

    tabnet

---
+++ classifier.py
@@ -0,0 +1,140 @@
+from pytorch_tabnet.tab_model import TabNetClassifier
+
+import torch
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_auc_score, mean_squared_error, accuracy_score
+
+import pandas as pd
+import numpy as np
+np.random.seed(0)
+
+import os
+import wget
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+# %matplotlib inline
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+dataset_name = 'census-income'
+out = Path(os.getcwd() + '/data/' + dataset_name + '.csv')
+out.parent.mkdir(parents=True, exist_ok=True)
+
+if out.exists():
+    print("File already exists.")
+else:
+    print("Downloading file...")
+    wget.download(url, out.as_posix())
+
+train = pd.read_csv(out)
+target = ' <=50K'
+if "Set" not in train.columns:
+    train["Set"] = np.random.choice(["train", "valid", "test"], p=[.8, .1, .1], size=(train.shape[0],))
+
+train_indices = train[train.Set == "train"].index
+valid_indices = train[train.Set == "valid"].index
+test_indices = train[train.Set == "test"].index
+
+nunique = train.nunique()
+types = train.dtypes
+
+categorical_columns = []
+categorical_dims =  {}
+for col in train.columns:
+    if types[col] == 'object' or nunique[col] < 200:
+        print(col, train[col].nunique())
+        l_enc = LabelEncoder()
+        train[col] = train[col].fillna("VV_likely")
+        train[col] = l_enc.fit_transform(train[col].values)
+        categorical_columns.append(col)
+        categorical_dims[col] = len(l_enc.classes_)
+    else:
+        train.fillna(train.loc[train_indices, col].mean(), inplace=True)
+
+
+# Categorical Embedding을 위해 Categorical 변수의 차원과 idxs를 담음.
+unused_feat = ['Set']
+features = [ col for col in train.columns if col not in unused_feat+[target]]
+cat_idxs = [ i for i, f in enumerate(features) if f in categorical_columns]
+cat_dims = [ categorical_dims[f] for i, f in enumerate(features) if f in categorical_columns]
+
+set_name = ['train', 'valid', 'test']
+
+for s in set_name:
+    globals()[f'X_{s}'] = train[features].values[globals()[f'{s}_indices']]
+    # globals()[f'y_{s}'] = train[target].values[globals()[f'{s}_indices']].reshape(-1, 1)
+    globals()[f'y_{s}'] = train[target].values[globals()[f'{s}_indices']]
+
+    print(f"X {s} shape :", globals()[f'X_{s}'].shape)
+
+    print(f"y {s} shape :", globals()[f'y_{s}'].shape)
+
+clf = TabNetClassifier(n_steps=5,
+                       cat_idxs=cat_idxs,
+                       cat_dims=cat_dims,
+                       cat_emb_dim=10,
+                       optimizer_fn=torch.optim.Adam,
+                       optimizer_params=dict(lr=1e-2),
+                       scheduler_params={"step_size":50,
+                                         "gamma":0.9},
+                       scheduler_fn=torch.optim.lr_scheduler.StepLR,
+                       mask_type='sparsemax' # "sparsemax", entmax
+                      )
+
+max_epochs = 5
+
+clf.fit(
+    X_train=X_train, y_train=y_train,
+    eval_set=[(X_train, y_train), (X_valid, y_valid)],
+    eval_name=['train', 'valid'],
+    eval_metric=['auc'],
+    max_epochs=max_epochs , patience=20,
+    batch_size=1024, virtual_batch_size=128,
+    num_workers=0,
+    weights=1,
+    drop_last=False,
+)
+
+preds = clf.predict_proba(X_test)
+test_auc = roc_auc_score(y_score=preds[:,1], y_true=y_test)
+
+plt.title("loss plot")
+plt.plot(clf.history['loss'])
+plt.show()
+
+# plot accuracy
+plt.title("AUC history")
+plt.plot(clf.history['train_auc'])
+plt.plot(clf.history['valid_auc'])
+plt.legend(['train', 'valid'])
+
+
+preds_mapper = { idx : class_name for idx, class_name in enumerate(clf.classes_)}
+
+preds = clf.predict_proba(X_test)
+
+y_pred = np.vectorize(preds_mapper.get)(np.argmax(preds, axis=1))
+
+test_acc = accuracy_score(y_pred=y_pred, y_true=y_test)
+
+print(f"BEST VALID SCORE FOR {dataset_name} : {clf.best_cost}")
+print(f"FINAL TEST SCORE FOR {dataset_name} : {test_acc}")
+
+
+# or you can simply use the predict method
+# y_pred = clf.predict(X_test)
+# test_acc = accuracy_score(y_pred=y_pred, y_true=y_test)
+# print(f"FINAL TEST SCORE FOR {dataset_name} : {test_acc}")
+
+
+feat_importances = clf.feature_importances_
+indices = np.argsort(feat_importances)
+plt.figure(figsize=(10,8))
+plt.title("Feature importances")
+plt.barh(range(len(feat_importances)), feat_importances[indices],
+       color="r", align="center")
+# If you want to define your own labels,
+# change indices to a list of labels on the following line.
+plt.yticks(range(len(feat_importances)), [features[idx] for idx in indices])
+plt.ylim([-1, len(feat_importances)])
+plt.show()
---
+++ tutorials_2.py
@@ -0,0 +1,146 @@
+from pytorch_tabnet_custom.tab_model import TabNetClassifier
+
+import torch
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_auc_score
+
+import pandas as pd
+import numpy as np
+np.random.seed(0)
+
+import os
+import wget
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+# %matplotlib inline
+
+
+'''
+Download census-income dataset
+'''
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+dataset_name = 'census-income'
+out = Path(os.getcwd()+'/data/'+dataset_name+'.csv')
+
+out.parent.mkdir(parents=True, exist_ok=True)
+if out.exists():
+    print("File already exists.")
+else:
+    print("Downloading file...")
+    wget.download(url, out.as_posix())
+
+
+'''
+Load data and split
+'''
+train = pd.read_csv(out)
+target = ' <=50K'
+if "Set" not in train.columns:
+    train["Set"] = np.random.choice(["train", "valid", "test"], p =[.8, .1, .1], size=(train.shape[0],))
+
+train_indices = train[train.Set=="train"].index
+valid_indices = train[train.Set=="valid"].index
+test_indices = train[train.Set=="test"].index
+
+
+'''
+Simple preprocessing
+'''
+nunique = train.nunique()
+types = train.dtypes
+
+categorical_columns = []
+categorical_dims =  {}
+for col in train.columns:
+    if types[col] == 'object' or nunique[col] < 200:
+        print(col, train[col].nunique())
+        l_enc = LabelEncoder()
+        train[col] = train[col].fillna("VV_likely")
+        train[col] = l_enc.fit_transform(train[col].values)
+        categorical_columns.append(col)
+        categorical_dims[col] = len(l_enc.classes_)
+    else:
+        train.fillna(train.loc[train_indices, col].mean(), inplace=True)
+
+
+'''
+Define categorical features for categorical embeddings
+'''
+unused_feat = ['Set']
+features = [ col for col in train.columns if col not in unused_feat+[target]]
+cat_idxs = [ i for i, f in enumerate(features) if f in categorical_columns]
+cat_dims = [ categorical_dims[f] for i, f in enumerate(features) if f in categorical_columns]
+
+X_train = train[features].values[train_indices]
+y_train = train[target].values[train_indices]
+
+X_valid = train[features].values[valid_indices]
+y_valid = train[target].values[valid_indices]
+
+X_test = train[features].values[test_indices]
+y_test = train[target].values[test_indices]
+
+
+'''
+Network parameters
+'''
+from pytorch_tabnet.pretraining import TabNetPretrainer
+# TabNetPretrainer
+unsupervised_model = TabNetPretrainer(
+    cat_idxs=cat_idxs,
+    cat_dims=cat_dims,
+    cat_emb_dim=3,
+    optimizer_fn=torch.optim.Adam,
+    optimizer_params=dict(lr=2e-2),
+    mask_type='entmax', # "sparsemax",
+    # n_shared_decoder=1, # nb shared glu for decoding
+    # n_indep_decoder=1, # nb independent glu for decoding
+)
+
+max_epochs = 10 if not os.getenv("CI", False) else 2
+
+unsupervised_model.fit(
+    X_train=X_train,
+    eval_set=[X_valid],
+    max_epochs=max_epochs , patience=5,
+    batch_size=2048, virtual_batch_size=128,
+    num_workers=0,
+    drop_last=False,
+    pretraining_ratio=0.8,
+)
+torch.Tensor(X_train).to(unsupervised_model.device).float()
+unsupervised_model.network.eval()
+embedded_x = unsupervised_model.network.embedder(torch.Tensor(X_train).to(unsupervised_model.device).float())
+steps_out, _ = unsupervised_model.network.encoder(embedded_x)
+res = 0
+
+feat_transformers = unsupervised_model.network.decoder.feat_transformers
+
+for step_nb, step_output in enumerate(steps_out):
+    x = feat_transformers[step_nb](step_output)
+    res = torch.add(res, x)
+
+res.shape
+
+unsupervised_model.extract_features(X_train)
+
+torch.Tensor(X_train)
+unsupervised_model.network.encoder(torch.Tensor(X_train))
+
+
+# Make reconstruction from a dataset
+reconstructed_X, embedded_X = unsupervised_model.predict(X_valid)
+assert(reconstructed_X.shape==embedded_X.shape)
+
+unsupervised_explain_matrix, unsupervised_masks = unsupervised_model.explain(X_valid)
+
+fig, axs = plt.subplots(1, 3, figsize=(20,20))
+
+for i in range(3):
+    # axs[i].imshow(unsupervised_masks[i][:50])
+    axs[i].imshow(unsupervised_masks[i][:50])
+    axs[i].set_title(f"mask {i}")
+
+plt.show()
+
